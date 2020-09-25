0a1b68195ec75fbfab843eb67a742b5055baa41a
Akanksha Pandey <akanksha.pandey@suiit.ac.in>
2020-09-25 20:15:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Board from './components/Board';
+import Menu from './components/Menu';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Menu/>
+      <Board/>
+    </div>
+  );
+}
+
+export default App;
